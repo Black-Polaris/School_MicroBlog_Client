e707cce9f1bd36a4892d88121ae88c197b5ec305
shenqinbin <599405389@qq.com>
2022-02-19 12:24:21 +0800

    cnpm install axios --save

--- microblog_vue/src/main.js
+++ microblog_vue/src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
