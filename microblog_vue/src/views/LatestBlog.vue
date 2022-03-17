b69db7ffbcb061d8cb6a3cebc93a6d237f853f08
shenqinbin <599405389@qq.com>
2022-03-18 01:29:59 +0800

    code review

--- microblog_vue/src/views/LatestBlog.vue
+++ microblog_vue/src/views/LatestBlog.vue
@@ -307,7 +307,7 @@ export default {
 .infinite-list-wrapper {
   width: 100%;
 
-  height: 790px;
+  height: 850px;
   overflow: auto;
   box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
 }
